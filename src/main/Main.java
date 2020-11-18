bc26c515f2323dca195f921b31a85b4d5c5e9942
doit-now <hoang.nguyenthe@gmail.com>
2020-11-18 09:06:19 +0700

    add JUnit code & .jar(s)

--- src/main/Main.java
+++ src/main/Main.java
@@ -39,3 +39,34 @@ public class Main {
     }
 
 }
+
+//MÌNH MÚN KẾT LUẬN HÀM NGON, MÌNH PHẢI TEST CÁC TRƯỜNG HỢP XÀI HÀM
+//TRƯỜNG HỢP 5! 0! -5! 21! 20! ....
+//mình phải thiết kế các test case, tình huống test để sau đó mình verify
+//giữa giá trị trả về của hàm (actual) có khớp với expected cái mình dự đoán
+//hàm chạy phải trả về
+//và nếu mọi thứ khớp, tức là actual == expected, mình mới dám gáy: HÀM NGON
+//CÁCH NÀY CÓ NHƯỢC ĐIỂM: NHÌN BẰNG MẮT VÀ SO SÁNH KẾT QUẢ CỦA TỪNG CASE 
+//                                                                  tình huống test
+//Cách này ổn, nhưng tiềm ẩn sai sót do nhìn = mắt nhiều case
+
+//CÁCH NÂNG CAO
+//CŨNG NHÌN = MẮT, NHƯNG CHỈ NHÌN 2 MÀU XANH - ĐỎ CHO MỌI TEST CASES
+//có nghĩa: nếu bạn xài hàm với 100 tham số, chạy hàm 100 tình huống khác nhau
+//bạn chỉ cần nhìn duy nhất 2 màu xanh, đỏ
+//XANH, CODE ỔN CHO MỌI TEST CASE, MỌI TÌNH HUỐNG GỌI HÀM
+//      MỌI CASE PHẢI CÓ EXPECTED = ACTUAL THÌ MỚI XANH
+//ĐỎ, MỌI CASE MÀU XANH, CHỈ CÓ ÍT NHẤT 1 THẰNG ĐỎ, KẾT LUẬN CẢ ĐÁM ĐỎ
+//      hàm đúng cho 99 trường hợp, expected = actual cho 99% tình huống
+//      chỉ có 1 tình huống sai, ko khớp, kết luận luôn, hàm ko ổn định
+//      ai dám xài khi ko ổn định, ổn định và chính xác phải cho mọi trường hợp
+//CHỈ CẦN ÍT NHẤT 1 CÁI KHÔNG ỔN, ĐỎ CẢ ĐÁM ĐỂ ĐẢM BẢO RẰNG HÀM PHẢI CHÍNH XÁC MỌI
+//TÌNH HUỐNG
+
+//MÚN LÀM ĐC ĐIỀU NÀY, CẦN SỰ PHỤ GIÚP CỦA CÁC THƯ VIỆN BÊN NGOÀI JDK, GỌI LÀ
+//UNIT TEST FRAMEWORK
+//CÁC BỘ UNIT TEST NỔI TIẾNG: JUnit, TestNG, NUnit, xUnit, PHPUnit, ...
+//bữa nay mình học xanh đỏ qua JUnit
+//vẫn với mục tiêu test hàm có ngon ko, nhưng chỉ nhìn đúng 2 màu, ko care chi
+//tiết các tình huống chạy hàm
+
